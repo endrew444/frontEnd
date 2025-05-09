ba9e07049e502b4231f03008bff51ff3e0f3dc5d
endrew444 <endrewmachado59@gmail.com>
2025-05-09 08:49:02 -0300

    09/05/25

--- Angular14/editor-perfil/src/app/form-usuario/form-usuario.component.ts
+++ Angular14/editor-perfil/src/app/form-usuario/form-usuario.component.ts
@@ -6,10 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-usuario.component.css']
 })
 export class FormUsuarioComponent {
+
+  //atributos
   nome: string = "";
   email: string = "";
   telefone: string = "";
   genero: string = "";
   idade: number | null = null;
   profissao: string = "";
+
+  //métodos
+  limparDados(){
+    this.nome = "";
+    this.email = "";
+    this.telefone="";
+    this.genero="";
+    this.idade = null;
+    this.profissao = "";
+  }
+
+  //validar os dados do formulario
+  validarDados() {
+    if (!this.email.includes('@') || this.idade === null || this.idade <= 17) {
+      alert("Erro no formulário");
+    } else {
+      alert("Dados válidos!");
+    }
+  }
+//enviar os dados do formulario
+  enviarFormulario() {
+    if (this.nome && this.email && this.telefone && this.genero && this.idade && this.profissao) {
+      alert("Formulário enviado com sucesso!");
+    } else {
+      alert("Preencha todos os campos obrigatórios.");
+    }
+  }
 }
