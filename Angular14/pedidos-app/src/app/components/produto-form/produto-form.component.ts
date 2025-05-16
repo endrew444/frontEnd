96c8cdd87a1d232242819895642490a50e6e9f3c
endrew444 <endrewmachado59@gmail.com>
2025-05-16 08:06:58 -0300

    16/05/25

--- Angular14/pedidos-app/src/app/components/produto-form/produto-form.component.ts
+++ Angular14/pedidos-app/src/app/components/produto-form/produto-form.component.ts
@@ -1,26 +1,25 @@
 import { Component } from '@angular/core';
 import { Produto } from 'src/app/models/produto.model';
 import { DadosService } from 'src/app/services/dados.service';
-import { Cliente } from 'src/app/models/cliente.model';
 
 @Component({
   selector: 'app-produto-form',
   templateUrl: './produto-form.component.html',
   styleUrls: ['./produto-form.component.scss']
 })
-
 export class ProdutoFormComponent {
-  nome = "";
-  preco = 0;
-  cliente: Cliente | null = null;
 
-  constructor(private dadoService: DadosService) { }
+  //atributos
+  nome :string = "";
+  preco :number = 0;
 
-  adicionarProduto() {
-    const produto = new Produto(this.dadoService.getProdutos().length+1, this.nome, this.preco);
-    this.dadoService.adicionarProduto(produto);
-    this.nome = "";
-    this.preco = 0;
+  constructor(private dadosService: DadosService){}
+
+  adicionarProduto(){
+    const produto = new Produto(this.dadosService.getProdutos().length+1, this.nome, this.preco);
+    this.dadosService.adicionarProduto(produto);
+    this.nome="";
+    this.preco=0;
   }
 
 }
